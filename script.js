907ed8399a4a68ee914530363290ddacbc6df818
laka35111-debug <laka35111@gmail.com>
2026-06-30 05:34:15 +0300

    Add searchUnits function for filtering cards

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function searchUnits() {
+    let input = document.getElementById("searchBox").value.toLowerCase();
+    let cards = document.querySelectorAll(".grid a");
 
+    cards.forEach(card => {
+        if (card.textContent.toLowerCase().includes(input)) {
+            card.style.display = "";
+        } else {
+            card.style.display = "none";
+        }
+    });
+}
