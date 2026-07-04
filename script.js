d3a03a5ad77cd7c7888e7caf916acccb72f6bbc6
laka35111-debug <laka35111@gmail.com>
2026-07-04 18:07:51 +0300

    Add Intersection Observer and scroll-to-top button
    Implement Intersection Observer for section visibility and add scroll-to-top button functionality.

--- script.js
+++ script.js
@@ -23,3 +23,31 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 
 });
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll("section").forEach(section=>{
+    section.classList.add("hidden");
+    observer.observe(section);
+});
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+});
+
+topBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
